80a4e548e68fdd649dcb8e30b28afa511fb592ac
Mohamed Foula <mohamed.m.foula@gmail.com>
2023-07-19 18:07:10 +0300

    fix: fixed the exports to be working

---
+++ index.ts
@@ -0,0 +1,14 @@
+
+import { ILimiterRepo } from './src/interfaces/limiter_repo_interface'
+import { RequestAttemptDocumentType } from './src/types/request_attempt_document_type'
+import { WindowsLimitsType } from './src/types/windows_limits_type'
+import { RequestAttemptType } from './src/types/request_attempt_type'
+import { RequestsLimiter } from './src/requests_limitter'
+
+export {
+  ILimiterRepo,
+  RequestAttemptDocumentType,
+  RequestAttemptType,
+  RequestsLimiter,
+  WindowsLimitsType,
+}
